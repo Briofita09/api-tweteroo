7d8ba61f511f8979bfe155d7df68268c20968c6d
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-04-18 12:20:08 -0300

    feat: sign-up route

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.listen(4000, () => console.log("Server up and running at port 4000"));
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+  const data = req.body;
+  users.push(data);
+  res.json({ msg: "OK" }).status(201);
+});
+
+app.listen(4000, () =>
+  console.log("Server up and running at port 4000", users)
+);
