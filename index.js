40733166d64b48c88123ac68d0b8c5e3dcd6eb76
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2022-04-18 14:00:10 -0300

    feat: post and get tweets routes implemented

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const app = express();
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
   const data = req.body;
   users.push(data);
   res.json({ msg: "OK" }).status(201);
+});
+
+app.post("/tweets", (req, res) => {
+  const data = req.body;
+  tweets.push(data);
+  res.json({ msg: "OK" }).status(201);
+});
+
+app.get("/tweets", (req, res) => {
+  const lastTweets = [];
+  for(let i = tweets.length>10? tweets.length-10: 0; i<tweets.length; i++){
+    const userIndex = users.findIndex(user => user.username === tweets[i].username);
+    lastTweets.push({...tweets[i], avatar: users[userIndex].avatar})
+  }
+  res.json(lastTweets).status(201);
 });
 
 app.listen(4000, () =>
